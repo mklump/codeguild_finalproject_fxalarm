be6b6dad932d9b729c82e4fab3755cbd233634fa
mklump <mklump@gmail.com>
2016-05-25 06:43:37 -0700

    format datasamples - python HtmlParser decision instead of XSLT that uses XML as input.

--- finalproject_fxalarm/finalproject_fxalarm/logic.py
+++ finalproject_fxalarm/finalproject_fxalarm/logic.py
@@ -4,6 +4,7 @@ Python Coding Bootcamp (pdxcodeguild)
 FXAlarm Final Project file finsalproject_fxalarm/logic.py
 by Matthew James K on 5/16/2016
 """
+from . import models
 
 class logic(object):
     """
