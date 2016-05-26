77744660c493032d815c4496e4a676497ec59b32
mklump <mklump@gmail.com>
2016-05-25 23:56:37 -0700

    debugging session, beautiful soup working great.

--- PythonTestApp/parse_fxalarm.py
+++ PythonTestApp/parse_fxalarm.py
@@ -9,14 +9,17 @@ from bs4 import BeautifulSoup
 def parse_html_source_file():
     soup_html_xml_parser = None
     try:
-        with open('..\static\finalproject_fxalarm\sample_data\primary_data_sample.html') as html:
-            soup_html_xml_parser = BeautifulSoup(html)
+        with open('primary_data_sample.html') as html:
+            soup_html_xml_parser = BeautifulSoup(html, 'html.parser')
         # end with block/close file
     except Exception as error:
         print(error.__cause__())
+    return soup_html_xml_parser
 
 def main():
-    parse_html_source_file()
+    html_parser = parse_html_source_file()
+    string_match = html_parser.find(string = 'EURUSD')
+    print(string_match + ' ' + string_match.next_element.next_element.contents[0]) # We found something!
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
