00666341cfae5c5a0fc82a686ab2811098f33273
Matthew James K <mklump@gmail.com>
2016-06-01 16:20:59 -0700

    working on dynamic functions

--- finalproject_fxalarm/finalproject_fxalarm/parse_fxalarm.py
+++ finalproject_fxalarm/finalproject_fxalarm/parse_fxalarm.py
@@ -4,6 +4,10 @@ FXAlarm Final Project file finsalproject_fxalarm/parse_fxalarm.py
 by Matthew James K on 5/25/2016
 """
 from bs4 import BeautifulSoup
+import requests
+from django.http import HttpRequest
+from django.http import HttpResponse
+from . import models
 import os
 
 def parse_html_source_file(input_file):
@@ -46,3 +50,59 @@ def get_next_usd_parse(input_file):
     html_parser = parse_html_source_file(input_file)
     return [ '%s' % parse_currency_node(html_parser, symbol) for symbol in usd_symbols ]
     
+def set_fxalarm_db_login(username, pwd, website):
+    """
+    This function can only be run currenlt from a python interactive console.
+    This function accepts 3 parameters, to save to the MyCredentials database table for access by
+    this program - username_as_email, password, and target_website.
+    :param 1: username as string to save to username_as_email
+    :param 2: pwd as string to save to password
+    :param 3: website as string to save to target_website
+    """
+    try:
+        my_credentials = models.MyCredentials(username_as_email = username,
+            password = pwd,
+            target_website = website,)
+        my_credentials.save()
+    except Exception as error:
+        print(error)
+        raise RuntimeError(error)
+
+def open_fxalarm_session():
+    """
+    This function reads the MyCredentials table for the existance of 1 row, and starts a new active session.
+    """
+
+def get_and_keep_alive_realtime_data():
+    """
+    This function checks regularly that the current active session is still active, and calls parse function of realtime data.
+    http://stackoverflow.com/questions/1622793/django-cookies-how-can-i-set-them
+    """
+
+def close_fxalarm_session(request: HttpRequest) -> bool:
+    """
+    This function checks if a current fxalarm session is active, and closes it.
+    http://docs.python-requests.org/en/master/user/quickstart/#cookies
+    http://stackoverflow.com/questions/2489669/function-parameter-types-in-python
+    """
+    target_site = models.MyCredentials.target_website.objects.all()[0]
+    if 'UserID' in request.COOKIES and 'deleted' != request.COOKIES['UserID']:
+        req = requests.post('%slogout.php' % target_site)
+        req.cookies['UserID'] = 'deleted' # OR EXECUTE
+        response = HttpResponse('')
+        response.set_cookie('UserID', 'deleted')
+
+def set_cookie(response, key, value, days_expire=7):
+  if days_expire is None:
+    max_age = 365 * 24 * 60 * 60  #one year
+  else:
+    max_age = days_expire * 24 * 60 * 60 
+  expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+  response.set_cookie(key, value, max_age=max_age, expires=expires,
+                      domain=settings.SESSION_COOKIE_DOMAIN, secure=settings.SESSION_COOKIE_SECURE or None)
+
+# Use the following code before sending a response:
+def view(request: HttpRequest): # TODO: Write function to check what IP address the request object is running your code from!!!
+  response = HttpResponse("hello")
+  set_cookie(response, 'name', 'jujule')
+  return response
