ed5fa6e786756a2deb05dcc9bfa1fa50da7d6f67
mklump <mklump@gmail.com>
2016-06-01 07:54:27 -0700

    web request recordings round 1

--- finalproject_fxalarm/finalproject_fxalarm/static/finalproject_fxalarm/scripts/fxalarm_site.js
+++ finalproject_fxalarm/finalproject_fxalarm/static/finalproject_fxalarm/scripts/fxalarm_site.js
@@ -5,18 +5,43 @@ by: Matthew James K on 5/16/2016
 */
 'use strict'
 
+/*
+ * This function returns the usd_summary list of strings for processing.
+ * @returns {Array} Of strings that is the usd_summary of each data capture snapshot.
+ */
 function getUSDSummaryCollection() {
-  var summary = '{{ usd_summary }}'.replace(/&quot;/g,"\"");
-  return summary;
+  var summary = $('[name=\'usd_summary\']');
+  return summary[0].value;
 }
 
+/*
+ * This function returns the usd_summary list of strings for processing.
+ * @returns {Array} Of strings that is the usd_detail of each data capture snapshot.
+ */
 function getUSDDetailCollection() {
-
+  var detail = $('[name=\'usd_detail\']');
+  return detail[0].value;
 }
 
+/*
+ * This function registers the click event of the summary select item option list to reveal the details.
+ */
 function registerSelectOptionClick() {
-  var select_summary = $('#view_select_summary > option').on('click', function() {
-
+  $('#view_select_summary > option').on('click', function(event) {
+    var capture_summaries = getUSDSummaryCollection(); // Outputs "['This', 'inner', 'list']", and requires Array.from(arrayLike[, mapFn[, thisArg]])
+    var count = 0;
+    for (var line in capture_summaries) {
+      if (line === event.target.value)
+        break;
+      else
+        count++;
+    }
+    var summary;
+    for (summary = 0; summary < capture_summaries.length; summary++) {
+      if (capture_summaries[summary] === event.target.value)
+        break;
+    }
+    $('#view_item_detail').innerHTML = '';
   });
 }
 
@@ -33,7 +58,6 @@ function registerGlobalEventHandlers() {
  */
 function main() {
   registerGlobalEventHandlers();
-  getUSDSummaryCollection();
 }
 
 /*
